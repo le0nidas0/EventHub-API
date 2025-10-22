ba5541d55a394caccd966da4a001f6b067e9b4e5
le0nidas0 <leonidasmoreira098@gmail.com>
2025-10-22 18:35:20 -0300

    Initial commit

---
+++ src/main/java/com/example/EventHub_API/EventHubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventHub_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventHubApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventHubApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EventHub_API/EventHubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EventHub_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
